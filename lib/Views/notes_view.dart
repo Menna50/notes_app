6348f075792e2868a52bf2fa8393298c3b30b31f
DELL <DELL@MAYOYA>
2025-11-07 13:23:31 +0200

    create notes view

---
+++ lib/Views/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  static String id ='NotesView';
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
